f6294049c96dff907cc2d5464b9bc92b047db48f
Juwan Dobson <dobsonjuwan@yahoo.com>
2026-05-03 20:21:59 -0500

    Update Version

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "aksdevopsacrjdm0521" # must be globally unique
+  name                = "aksdevopsacrjdm0521" 
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
