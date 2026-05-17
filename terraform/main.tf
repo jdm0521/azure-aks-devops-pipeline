b98046498bdb0f7ecdfe7d017e466f05916940e7
Juwan Dobson <dobsonjuwan@yahoo.com>
2026-05-16 20:06:16 -0500

    Add Key Vault integration and Azure Monitor documentation

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,21 @@ provider "azurerm" {
   features {}
 }
 
+
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_resource_group" "rg" {
   name     = "rg-aks-devops"
   location = "eastus"
+}
+
+
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "law-aks-devops"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
 }
 
 resource "azurerm_container_registry" "acr" {
@@ -29,5 +41,35 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   identity {
     type = "SystemAssigned"
+  }
+
+  key_vault_secrets_provider {
+    secret_rotation_enabled = true
+  }
+  
+  oms_agent {
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+}
+
+}
+
+
+resource "azurerm_key_vault" "kv" {
+  name                        = "kv-jdm0521-devops"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  sku_name                    = "standard"
+  purge_protection_enabled    = false
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    secret_permissions = [
+      "Get",
+      "Set",
+      "List"
+    ]
   }
 }
